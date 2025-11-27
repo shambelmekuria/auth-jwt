6c58598fd4df83f95b21fd5da71ac116a3d4b29d
shambel mekuria <shambelmekuria2022@gmail.com>
2025-11-27 10:34:37 +0300

    edited

--- api/api/settings.py
+++ api/api/settings.py
@@ -122,7 +122,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
     
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.CookieJWTAuthentication(',
     )
    
 }
--- api/users/authentication.py
+++ api/users/authentication.py
@@ -1,1 +1,5 @@
 from rest_framework_simplejwt.authentication import  JWTAuthentication
+
+
+class CookieJWTAuthentication(JWTAuthentication):
+    pass
